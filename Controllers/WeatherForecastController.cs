f149b604aee541f3793150f8ea162b533412d283
Kaoru Yamamoto <k_yamamoto@casareal.co.jp>
2023-12-07 14:05:00 +0900

    Add XML Comments.

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -2,6 +2,9 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace NSwagSample.Controllers;
 
+/// <summary>
+/// 気象状況のWeb API
+/// </summary>
 [ApiController]
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
@@ -13,11 +16,19 @@ public class WeatherForecastController : ControllerBase
 
     private readonly ILogger<WeatherForecastController> _logger;
 
+    /// <summary>
+    /// コンストラクター
+    /// </summary>
+    /// <param name="logger">ログの記録に用いるロガー・オブジェクト</param>
     public WeatherForecastController(ILogger<WeatherForecastController> logger)
     {
         _logger = logger;
     }
 
+    /// <summary>
+    /// 気象状況の一覧を取得するためのエンド・ポイント
+    /// </summary>
+    /// <returns>気象状況の一覧</returns>
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -1,12 +1,27 @@
 namespace NSwagSample;
 
+/// <summary>
+/// 気象状況
+/// </summary>
 public class WeatherForecast
 {
+    /// <summary>
+    /// 観測日時
+    /// </summary>
     public DateTime Date { get; set; }
 
+    /// <summary>
+    /// 気温（摂氏）
+    /// </summary>
     public int TemperatureC { get; set; }
 
+    /// <summary>
+    /// 気温（華氏）
+    /// </summary>
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
+    /// <summary>
+    /// 概況
+    /// </summary>
     public string? Summary { get; set; }
 }
