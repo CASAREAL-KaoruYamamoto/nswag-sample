8e947bdf5b51151db345b870d6fcf00b277079d4
Kaoru Yamamoto <k_yamamoto@casareal.co.jp>
2023-12-07 13:43:15 +0900

    Add API Information.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using NSwag;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +7,28 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddOpenApiDocument();
+builder.Services.AddOpenApiDocument(options => {
+    options.PostProcess = document =>
+    {
+        document.Info = new OpenApiInfo
+        {
+            Version = "v1",
+            Title = "NSwag Sample",
+            Description = "An ASP.NET Core Web API for NSwag Sample",
+            TermsOfService = "https://example.com/terms",
+            Contact = new OpenApiContact
+            {
+                Name = "Example Contact",
+                Url = "https://example.com/contact"
+            },
+            License = new OpenApiLicense
+            {
+                Name = "Example License",
+                Url = "https://example.com/license"
+            }
+        };
+    };
+});
 
 var app = builder.Build();
 
